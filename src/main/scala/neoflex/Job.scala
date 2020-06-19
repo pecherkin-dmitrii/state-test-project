6ba395129356ce7fc3a363a4285f3c0eb8ed906a
dpecherkin <dpecherkin@neoflex.ru>
2020-06-19 17:49:10 +0400

    refactoring

--- src/main/scala/neoflex/Job.scala
+++ src/main/scala/neoflex/Job.scala
@@ -2,7 +2,7 @@ package neoflex
 
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
-import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor, MapState, MapStateDescriptor, ValueState, ValueStateDescriptor}
 import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
@@ -12,8 +12,6 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import scala.util.control.Exception.{catching, nonFatalCatcher}
 
 object Job {
-
-  case class TestClass(id: String, info: String)
 
   def main(args: Array[String]) {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -25,7 +23,7 @@ object Job {
 
     val dataStream = env
       .socketTextStream("localhost", 9999)
-      .keyBy(_.length)
+      .keyBy(_.length.toString)
       .map(new MyMapFunction()).uid("testOperator")
 
     dataStream.print()
@@ -34,28 +32,63 @@ object Job {
   }
 
   class MyMapFunction extends RichMapFunction[String, String] {
-    val testModelsListStateDescriptor =
-      new ListStateDescriptor[String](
+    val testListStateDescriptor: ListStateDescriptor[String] = {
+      val descriptor = new ListStateDescriptor[String](
         "testStringListState",
         classOf[String]
       )
+      descriptor.setQueryable("testQueryableListState")
+      descriptor
+    }
+
+    val testValueStateDescriptor: ValueStateDescriptor[String] = {
+      val descriptor = new ValueStateDescriptor[String](
+        "testValueState",
+        classOf[String]
+      )
+      descriptor.setQueryable("testQueryableValueState")
+      descriptor
+    }
+
+    val testMapStateDescriptor: MapStateDescriptor[String, Int] = {
+      val descriptor = new MapStateDescriptor[String, Int](
+        "testMapState",
+        classOf[String],
+        classOf[Int]
+      )
+      descriptor.setQueryable("testQueryableMapState")
+      descriptor
+    }
 
     private var listState: ListState[String] = _
+    private var valueState: ValueState[String] = _
+    private var mapState: MapState[String, Int] = _
 
     override def open(parameters: Configuration): Unit = {
-      listState = getRuntimeContext.getListState(testModelsListStateDescriptor)
+      listState = getRuntimeContext.getListState(testListStateDescriptor)
+      valueState = getRuntimeContext.getState(testValueStateDescriptor)
+      mapState = getRuntimeContext.getMapState(testMapStateDescriptor)
     }
 
     override def map(value: String): String = {
       catching(nonFatalCatcher).either {
-        println(s"flatMap method. sentence = $value")
-        Thread.sleep(10000)
+        println(s"Map method. sentence = $value")
+
         println("list state:")
         listState.get().forEach(println)
+
+        println("value state:")
+        println(valueState.value())
+
+        println("map state:")
+        mapState.entries().forEach(entry => println(s"Key=${entry.getKey}, value=${entry.getValue}"))
+
+        println("Adding new value to all states:")
         listState.add(value)
+        valueState.update(value)
+        mapState.put(value, value.length)
       }.left.foreach(e => println(e))
       value
     }
   }
-
 }
